3775a3ca1fa9fa2cd3dd535f309598122a0cb988
Gisele Fox <72312641+giselejfox@users.noreply.github.com>
2023-05-06 17:26:44 -0700

    create simple home component

---
+++ src/components/Home.js
@@ -0,0 +1,18 @@
+import React from "react"
+
+function Home() {
+    return (
+    <section>
+      <div className="jumbotron jumbotron-fluid jumbos" id="main-jumbo">
+        <h1>Hello Students</h1>
+      </div>
+      <div>
+        {/* <!-- Short intro to page --> */}
+        <p className="lead text-center">Welcome to your final destination to events on campus</p>
+      </div>
+    </section>
+    )
+
+}
+
+export default Home
--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
+// CSS
+import "bootstrap/dist/css/bootstrap.css";
 import './index.css';
+
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
