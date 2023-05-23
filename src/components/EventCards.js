a58b93160704baa6946bc111af34f84b9874c786
Gisele Fox <72312641+giselejfox@users.noreply.github.com>
2023-05-22 20:02:32 -0700

    tried adding masonry for cards

--- src/components/EventCards.js
+++ src/components/EventCards.js
@@ -33,7 +33,7 @@ function formatDate(date) {
 function ClubTags({ clubName }) {
     // TODO make the tag clickable and lead to their page
     return (
-        <div className="col-auto d-flex flex-row rounded-pill align-items-center py-1 me-2 ps-0" >
+        <div className="col-auto d-flex flex-row rounded-pill align-items-center py-1 ps-0" >
             <div className="rounded-circle me-2" style={{backgroundColor: "red", height: .8+"rem", width:.8+"rem"}}></div>
             <div className="me-1 fw-bold" style={{fontSize: 12+"px"}}>{clubName}</div>
         </div>
@@ -56,43 +56,13 @@ function SingleEventCardImageTop({ event }) {
                     </div>
                 </div>
                 <div className="card-body text-start">
-                    <p className="card-subtitle grey-text">{dateTimeString}</p>
-                    <h5 className="card-title fw-bold fs-3">{event.eventName}</h5>
-                    <p className="card-text">{event.description}</p>
+                    <p className="card-subtitle" style={{color: "#585C63"}}>{dateTimeString}</p>
+                    <h5 className="card-title fw-bold" style={{fontSize: 1.25+"rem"}}>{event.eventName}</h5>
+                    <p className="card-text" style={{color: "#585C63"}}>{event.description}</p>
                     <div className="container">
                         <div className="row align-items-center">
                             <ClubTags clubName={event.clubName} />
                             <CardTags tags={event.tags} />
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    )
-}
-
-function SingleEventCardImageSide({ event }) {
-    let dateTimeString = formatDate(event.dateTime)
-    return (
-        <div className="event-card col-sm-6 col-md-6">
-            <div className="card border-1">
-                <div className="row g-0">
-                    <div className="darkener col-md-3" style={{height: "100%"}}>
-                        <div className="rounded m-3" style={{ backgroundImage: `url(${event.img})`, backgroundSize: "cover", backgroundPosition: "center", height: 100+"%", paddingTop: "100%", position:"relative"}} alt="...">
-                            <div className="overlay"></div>
-                        </div>
-                    </div>
-                    <div className="col-md-9">
-                        <div className="card-body text-start py-4">
-                            <p className="card-subtitle" style={{color: "#585C63"}}>{dateTimeString}</p>
-                            <h5 className="card-title fw-bold" style={{fontSize: 1.25+"rem"}}>{event.eventName}</h5>
-                            <p className="card-text" style={{color: "#585C63"}}>{event.description}</p>
-                            <div className="container">
-                                <div className="row align-items-center">
-                                    <ClubTags clubName={event.clubName} />
-                                    <CardTags tags={event.tags} />
-                                </div>
-                            </div>
                         </div>
                     </div>
                 </div>
@@ -124,11 +94,11 @@ function EventCards({ events }) {
     shownEvents = sortByDateTimeAscending(shownEvents)
 
     const allEventCards = shownEvents.map((event, index) => {
-        return <SingleEventCardImageSide key={index} event={event}/>
+        return <SingleEventCardImageTop key={index} event={event}/>
     })
     
     return (
-        <div className="row row-cols-1 row-cols-md-3 g-4">
+        <div className="row row-cols-1 row-cols-md-3 g-4" data-masonry='{"percentPosition": true }'>
             {allEventCards}
         </div>
     )
@@ -138,3 +108,42 @@ function EventCards({ events }) {
 export default EventCards
 
 
+// function SingleEventCardImageSide({ event }) {
+//     let dateTimeString = formatDate(event.dateTime)
+//     return (
+//         <div className="event-card col-sm-6 col-md-6">
+//             <div className="card border-1">
+//                 <div className="row g-0">
+//                     <div className="darkener col-md-3" >
+//                         <div
+//                         className="rounded m-3"
+//                         style={{
+//                             backgroundImage: `url(${event.img})`,
+//                             backgroundSize: "cover",
+//                             backgroundPosition: "center",
+//                             paddingTop: "100%",
+//                             position: "relative",
+//                         }}
+//                         alt="..."
+//                         >
+//                             <div className="overlay"></div>
+//                         </div>
+//                     </div>
+//                     <div className="col-md-9">
+//                         <div className="card-body text-start py-4">
+//                             <p className="card-subtitle" style={{color: "#585C63"}}>{dateTimeString}</p>
+//                             <h5 className="card-title fw-bold" style={{fontSize: 1.25+"rem"}}>{event.eventName}</h5>
+//                             <p className="card-text" style={{color: "#585C63"}}>{event.description}</p>
+//                             <div className="container">
+//                                 <div className="row align-items-center">
+//                                     <ClubTags clubName={event.clubName} />
+//                                     <CardTags tags={event.tags} />
+//                                 </div>
+//                             </div>
+//                         </div>
+//                     </div>
+//                 </div>
+//             </div>
+//         </div>
+//     )
+// }
